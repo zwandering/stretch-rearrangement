42cb7325be405fd078010469af2996c8a281558c
Haokun Zhu <zhkwandering@gmail.com>
2026-04-20 00:44:41 -0400

    update

--- src/exploration_rearrangement/exploration_rearrangement/manipulation/ik_ros_utils.py
+++ src/exploration_rearrangement/exploration_rearrangement/manipulation/ik_ros_utils.py
@@ -15,7 +15,7 @@ READY_POSE_P1 = {
 
 READY_POSE_P2 = {
     'joint_lift': 0.8,
-    'joint_arm_l0': 0.0,
+    'wrist_extension': 0.0,
     'joint_wrist_yaw': 0.0,
     'joint_wrist_pitch': -0.1,
     'gripper_aperture': 0.8,
